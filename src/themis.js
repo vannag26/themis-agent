81751f5ec0ea94c45ce1158cf3a07036340ea93d
vannag26 <vanna@vdgmanagement.com>
2026-04-05 13:57:28 -0400

    Update themis.js

--- src/themis.js
+++ src/themis.js
@@ -184,6 +184,13 @@ process.once('SIGTERM', () => bot.stop('SIGTERM'));
 const PORT = process.env.PORT || 3000;
 http.createServer((req, res) => res.end('Themis is alive')).listen(PORT, () => {
   console.log('keepalive server on :' + PORT);
+  const host = process.env.RENDER_EXTERNAL_HOSTNAME || ('localhost:' + PORT);
+  const isLocal = host.startsWith('localhost');
+  const pinger = isLocal ? http : require('https');
+  setInterval(() => {
+    const url = (isLocal ? 'http://' : 'https://') + host + '/';
+    pinger.get(url, (r) => console.log('keep-alive: ' + r.statusCode)).on('error', (e) => console.log('keep-alive err: ' + e.message));
+  }, 840000);
 });
 
 bot.launch({ dropPendingUpdates: true }).then(() => {
